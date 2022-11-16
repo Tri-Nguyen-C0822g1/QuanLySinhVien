58b04ea69d6b4a5187d8a28b43a8b9c767a0ac37
Tri-Nguyen-C0822g1 <tringuyen10396@gmail.com>
2022-11-16 16:31:53 +0700

    add new file

---
+++ query.sql
@@ -0,0 +1,37 @@
+create database quanlysinhvien;
+
+use quanlysinhvien;
+
+create table class(
+ClassID int not null auto_increment primary key,
+ClassName varchar(60) not null,
+StartDate Date not null,
+Status bit
+);
+
+create table student(
+StudentID int not null primary key auto_increment,
+StudentName varchar(30) not null,
+Address varchar(50),
+Phone int(20),
+Status bit,
+ClassID int not null,
+foreign key (ClassID) references Class (ClassID)
+);
+
+create table Subject(
+SubID int not null primary key auto_increment,
+SubName varchar(30) not null,
+Credit tinyint not null default 1  check ( Credit >=1 ),
+Status bit default 1
+);
+
+create table Mark(
+MarkID int not null primary key auto_increment,
+SubID int not null,
+StudentID int not null,
+Mark float default 0 check (Mark between 0 and 100 ),
+ExamTimes tinyint default 1,
+foreign key (SubID) references Subject (SubID),
+foreign key (StudentID) references Student (StudentID)
+);
